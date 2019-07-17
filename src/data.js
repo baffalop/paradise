4025b3ee58dcc2327853fc704d0af8681dee7b91
Nikita Gaidakov <ngaidakov@podfather.com>
2019-07-17 23:19:25 +0100

    Create data.js with exportˆ

---
+++ src/data.js
@@ -0,0 +1,15 @@
+const audio = {
+  basePath: '../www/audio/',
+  files: [
+    {
+      path: 'intro.mp3',
+      tail: 1.1,
+    },
+    {
+      path: 'ficus.mp3',
+      tail: 9.0,
+    },
+  ]
+}
+
+export { audio }
