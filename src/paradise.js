a1df0e94ad5c283e7a3eb69d671566784b7732c4
Nikita Gaidakov <ngaidakov@podfather.com>
2019-07-28 21:03:35 +0100

    Replace default cordova device ready with swipe and controlsˆ

--- src/paradise.js
+++ src/paradise.js
@@ -1,22 +1,24 @@
 import { audio as audioData } from 'data'
+import Swipe from 'swipejs'
 
 class Paradise {
   listen () {
+    this.swipe = new Swipe(document.getElementById('slider'), {
+      draggable: true,
+      speed: 300,
+      continuous: false,
+      callback: (index) => console.log(`Swipe pos: ${index}`),
+    })
+
     document.addEventListener('deviceready', this.init.bind(this));
   }
 
   init () {
-    const parentElement = document.querySelector('.app')
-    const listeningElement = parentElement.querySelector('.listening')
-    const receivedElement = parentElement.querySelector('.received')
-
-    listeningElement.setAttribute('style', 'display:none;')
-    receivedElement.setAttribute('style', 'display:block;')
-
     for (const file of audioData.files) {
       console.log(file.path)
     }
   }
 }
 
-(new Paradise()).listen()
+window.paradise = new Paradise()
+paradise.listen()
