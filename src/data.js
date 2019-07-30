a9935a706e5f7aac71c3e53031f6aed3173069aa
Nikita Gaidakov <ngaidakov@podfather.com>
2019-07-30 22:42:52 +0100

    Set basePath for each blockˆ

--- src/data.js
+++ src/data.js
@@ -1,11 +1,13 @@
 import Block from 'block'
 
 const audio = {
-  basePath: '../www/audio/',
+  basePath: '../www/dummy/audio/',
   blocks: [
     new Block('intro.mp3', 1.1),
     new Block('ficus.mp3', 9.0),
   ]
 }
+
+audio.blocks.map(block => block.basePath = audio.basePath)
 
 export { audio }
