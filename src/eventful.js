3d99f181a70e0206045c440e6351923c798a271a
Nikita Gaidakov <ngaidakov@podfather.com>
2019-08-16 11:38:31 +0100

    Add one shot events to Eventfulˆ

--- src/eventful.js
+++ src/eventful.js
@@ -5,6 +5,10 @@
  * @type {Eventful}
  */
 class Eventful {
+  constructor () {
+    this.oneShotEvents = {}
+  }
+
   /**
    * Set upstream: object which receives our emitted events
    *
@@ -36,7 +40,39 @@ class Eventful {
   receive (type, emitter, data) {
     if (typeof this.log === 'function') this.log(`received event '${type}'`)
     if (typeof this.handleEvent === 'function') this.handleEvent(type, emitter, data)
+    this.handleOneShot(type, emitter, data)
     this.emit(type, data, emitter)
+  }
+
+  /**
+   * @param {String} type
+   * @param {function} handler
+   * @param {Eventful} matchEmitter
+   */
+  addOneShotEvent (type, handler, matchEmitter = null) {
+    this.oneShotEvents[type] = {handler: handler}
+    if (matchEmitter) this.oneShotEvents[type].matchEmitter = matchEmitter
+  }
+
+  handleOneShot (type, emitter, data) {
+    if (!this.oneShotIsCallable(type)) return
+
+    const eventObject = this.oneShotEvents[type]
+    if (eventObject.hasOwnProperty('matchEmitter') && !eventObject.matchEmitter !== emitter) {
+      if (typeof this.log === 'function') this.log(`event '${type}' has one shot handler but emitter does not match`)
+      return
+    }
+
+    eventObject.handler(emitter, data)
+    this.oneShotEvents[type] = {}
+  }
+
+  /**
+   * @param {String} type
+   * @returns {boolean}
+   */
+  oneShotIsCallable (type) {
+    return this.oneShotEvents.hasOwnProperty(type) && typeof this.oneShotEvents[type].handler === 'function'
   }
 }
 
