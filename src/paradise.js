e10a63c75ced531f6168871c14b42a46443929e5
Nikita Gaidakov <ngaidakov@podfather.com>
2019-08-20 18:50:49 +0300

    Add music controls (+ dedicated playPause and skip methods in Paradise)

--- src/paradise.js
+++ src/paradise.js
@@ -24,11 +24,12 @@ class Paradise extends Eventful {
    * Do any DOM setup not dependent on deviceready, listen for deviceready event
    */
   listen () {
+    this.playPauseButton = document.getElementsByClassName('playpause')[0]
     this.swipe = new Swipe(document.getElementById('slider'), {
       draggable: true,
       speed: 300,
       continuous: false,
-      callback: (index) => console.log(`Swipe pos: ${index}`),
+      callback: index => console.log(`Swipe pos: ${index}`),
     })
 
     document.addEventListener('deviceready', this.init.bind(this));
@@ -57,6 +58,97 @@ class Paradise extends Eventful {
 
     this.player = new Player(playlist, audioData.playerOpts)
     this.player.setUpstream(this)
+  }
+
+  /**
+   * Toggle play/pause. If called from within app, should also create/destroy music controls.
+   *
+   * @param {Boolean} fromWithinApp
+   */
+  playPause (fromWithinApp = true) {
+    this.doWithDebounce(() => {
+      if (!this.playing) {
+        this.playing = true
+        this.playPauseButton.classList.add('pause')
+        this.player.play()
+        if (fromWithinApp) this.startControls()
+      } else {
+        this.playing = false
+        this.playPauseButton.classList.remove('pause')
+        this.player.pause()
+        if (fromWithinApp) this.destroyControls()
+      }
+
+      if (!fromWithinApp) MusicControls.updateIsPlaying(this.playing)
+    })
+  }
+
+  skipBack () {
+    if (this.playing) this.doWithDebounce(() => this.player.skip(-1))
+  }
+
+  skipForward () {
+    if (this.playing) this.doWithDebounce(() => this.player.skip())
+  }
+
+  startControls () {
+    MusicControls.create({
+        hasSkipForward: true,
+        hasSkipBackward: true,
+        skipForwardInterval: 10,
+        skipBackwardInterval: 10,
+
+        album: "I Don't Know Where Paradise Is",
+      },
+      () => this.log('started music controls'),
+      () => this.log('error starting music controls')
+    )
+
+    MusicControls.subscribe(this.handleControlsEvent.bind(this))
+    MusicControls.listen()
+  }
+
+  destroyControls () {
+    MusicControls.destroy(
+      () => this.log('destroyed music controls'),
+      () => this.log('error destroying music controls')
+    )
+  }
+
+  /**
+   * @param {String} action
+   */
+  handleControlsEvent (action) {
+    const message = JSON.parse(action).message
+    this.log(`received controls event ${message}`)
+
+    switch (message) {
+      case 'music-controls-toggle-play-pause':
+        this.playPause()
+        break
+      case 'music-controls-play':
+        if (this.playing) {
+          this.log('received controls play event but already playing')
+          MusicControls.updateIsPlaying(this.playing)
+          break
+        }
+        this.playPause()
+        break
+      case 'music-controls-pause':
+        if (!this.playing) {
+          this.log('received controls pause event but already paused')
+          MusicControls.updateIsPlaying(this.playing)
+          break
+        }
+        this.playPause()
+        break
+      case 'music-controls-next':
+        this.skipForward()
+        break
+      case 'music-controls-previous':
+        this.skipBack()
+        break
+    }
   }
 
   /**
@@ -94,32 +186,9 @@ class Paradise extends Eventful {
   }
 
   setupButtons() {
-    this.setButtonClick('playpause', e => {
-        const elem = e.target
-        if (!this.playing) {
-          this.playing = true
-          elem.classList.add('pause')
-          this.doWithDebounce(() => this.player.play())
-        } else {
-          this.playing = false
-          elem.classList.remove('pause')
-          this.doWithDebounce(() => this.player.pause())
-        }
-      })
-
-    this.setButtonClick(
-      'ffw',
-      e => {
-        if (this.playing) this.doWithDebounce(() => this.player.skip())
-      }
-    )
-
-    this.setButtonClick(
-      'rew',
-      e => {
-        if (this.playing) this.doWithDebounce(() => this.player.skip(-1))
-      }
-    )
+    this.setButtonClick('playpause', e => this.playPause())
+    this.setButtonClick('ffw', e => this.skipForward())
+    this.setButtonClick('rew', e => this.skipBack())
   }
 
   /**
