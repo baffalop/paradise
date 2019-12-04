10965d759144738f3322eea407f3f761a9493152
Nikita Gaidakov <ngaidakov@podfather.com>
2019-12-04 22:29:10 +0000

    Create Controls and Player components

---
+++ src/component/Controls.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="controls">
+    <div class="controlButton rew" @click="$emit('rew')" />
+    <div
+      @click="$emit('toggle-play')"
+      :class="playing ? 'pause' : 'play'"
+      class="controlButton playpause"
+    />
+    <div class="controlButton ffw" @click="$emit('ffw')" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'controls',
+    props: {
+      playing: {
+        type: Boolean,
+        required: true,
+      },
+    },
+    methods: {
+      rew () {
+        this.$emit('rew')
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .controls {
+    max-width: 500px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  }
+
+  .controlButton {
+    cursor: pointer;
+    height: 80px;
+    min-width: 80px;
+    background: transparent no-repeat;
+    background-size: 100% auto;
+    transition: opacity 80ms ease-in 100ms;
+  }
+
+  .controlButton:active {
+    transition: none;
+    opacity: 0.5;
+  }
+
+  .ffw {
+    transform: translate(-8px);
+    background-image: url(./../assets/img/wind-fw.png);
+  }
+
+  .rew {
+    transform: translateY(16px);
+    background-image: url(./../assets/img/wind-back.png);
+  }
+
+  .playpause {
+    background-image: url(./../assets/img/play-pause.png);
+    width: 160px;
+    background-size: 100% auto;
+  }
+
+  .playpause.play {
+    background-position: 50% 0px;
+  }
+
+  .playpause.pause {
+    background-position: 50% -80px;
+  }
+</style>
---
+++ src/component/Player.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="player">
+    <timeline :total="playlistCount" :current="currentIndex" :playing="playing" />
+    <controls
+        :playing="playing"
+        @toggle-play="togglePlay()"
+        @rew="rew()"
+        @ffw="ffw()"
+    />
+  </div>
+</template>
+
+<script>
+  import Timeline from './Timeline'
+  import Controls from './Controls'
+
+  export default {
+    name: 'Player',
+    components: {Controls, Timeline },
+    data: () => ({
+      playlistCount: 8,
+      currentIndex: 0,
+      playing: false,
+    }),
+    methods: {
+      rew () {
+        if (this.currentIndex <= 0) return
+        this.currentIndex --
+      },
+      ffw () {
+        if (this.currentIndex >= this.playlistCount - 1) return
+        this.currentIndex ++
+      },
+      togglePlay () {
+        this.playing = !this.playing
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .player {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .player > * {
+    padding: 15px 0 15px 0;
+  }
+</style>
