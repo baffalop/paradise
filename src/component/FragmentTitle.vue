9e4bae5fc4646ef164523b3fffb8b2a974a10148
Nikita Gaidakov <ngaidakov@podfather.com>
2019-12-05 13:18:33 +0000

    Need to define classes for all titles so images can be compiled

--- src/component/FragmentTitle.vue
+++ src/component/FragmentTitle.vue
@@ -3,9 +3,9 @@
     <transition name="swipe">
       <div
         class="title"
-        v-for="title in [name]"
-        :key="title"
-        :style="{ backgroundImage: imgFromName(title) }"
+        v-for="name in [name]"
+        :key="name"
+        :class="name"
       />
     </transition>
   </div>
@@ -19,11 +19,6 @@
         type: String,
         required: true,
       },
-    },
-    methods: {
-      imgFromName (name) {
-        return `url(./../assets/img/fragments/${name}.png)`
-      }
     },
   }
 </script>
@@ -45,6 +40,8 @@
     transition: transform 0.6s, opacity 0.6s;
   }
 
+  /* Transitions */
+
   .swipe-enter {
     transform: translate(100px, -20px);
     opacity: 0;
@@ -63,5 +60,63 @@
   .swipe-leave-active {
     transition-timing-function: ease-in, ease-out;
     transition-delay: 0s;
+  }
+
+  /* Titles */
+
+  .bathhouse {
+    background-image: url(./../assets/img/fragments/bathhouse.png);
+  }
+
+  .biographies {
+      background-image: url(./../assets/img/fragments/biographies.png);
+  }
+
+  .clocks {
+      background-image: url(./../assets/img/fragments/clocks.png);
+  }
+
+  .duster {
+      background-image: url(./../assets/img/fragments/duster.png);
+  }
+
+  .exlibris {
+    background-image: url(./../assets/img/fragments/exlibris.png);
+  }
+
+  .faggot {
+      background-image: url(./../assets/img/fragments/faggot.png);
+  }
+
+  .ficus {
+    background-image: url(./../assets/img/fragments/ficus.png);
+  }
+
+  .futurelovers {
+      background-image: url(./../assets/img/fragments/futurelovers.png);
+  }
+
+  .illustration {
+      background-image: url(./../assets/img/fragments/illustration.png);
+  }
+
+  .intro {
+    background-image: url(./../assets/img/fragments/intro.png);
+  }
+
+  .mirror {
+    background-image: url(./../assets/img/fragments/mirror.png);
+  }
+
+  .novels {
+    background-image: url(./../assets/img/fragments/novels.png);
+  }
+
+  .postcard {
+    background-image: url(./../assets/img/fragments/postcard.png);
+  }
+
+  .spiderplant {
+      background-image: url(./../assets/img/fragments/spiderplant.png);
   }
 </style>
